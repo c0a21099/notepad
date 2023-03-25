8f15eb93b76d311cdbc11f94dbf17d6a63abc1bf
knish <c0a21099e4@edu.teu.ac.jp>
2023-03-25 17:01:53 +0900

    ファイルの保存機能まで作成済み

--- notepad.py
+++ notepad.py
@@ -5,15 +5,25 @@ def open_file():
     # to do
     filename = tkf.askopenfilename(
         title = "open the txt file.",
-        filetypes = [('txt files', '*.txt')],
+        filetypes = [('txt files', '.txt')],
         initialdir = "./", # Your directory.
     )
-    print(filename)
+    # with open(filename, 'r') as f:
+    #     f.readline()
+    # print(filename)
     
 
 def save_file():
-    pass
     # to do
+    filename = tkf.asksaveasfilename(
+        title = "save the txt file.",
+        filetypes = [('txt files', '.txt')],
+        initialdir = "./", # Your directory.
+        defaultextension = "txt",
+    )
+    with open(filename, 'w') as f:
+        f.write(text.get("1.0", tk.END)) # Writes input to a text file.
+    print(filename)
 
 root = tk.Tk()
 root.title("Notepad")
@@ -23,14 +33,17 @@ menu_bar = tk.Menu(root)
 root.config(menu = menu_bar)
 
 # Create a filebar.
-file_menu = tk.Menu(menu_bar, tearoff = 0)
+file_menu = tk.Menu(menu_bar, tearoff = 0) # tearoff(メニューの切り取り可否) -> filebarを別枠に表示させない.
 menu_bar.add_cascade(label = "File", menu = file_menu)
 
 # Crate commands in the file bar.
-file_menu.add_command(label = "open", command = open_file)
-file_menu.add_command(label = "save", command = save_file)
+file_menu.add_command(label = "open", command = open_file, accelerator = "Ctrl+O")
+file_menu.add_command(label = "save", command = save_file, accelerator = "Ctrl+s")
 file_menu.add_separator() # Separator Line.
-file_menu.add_command(label = "exit", command = root.quit)
+file_menu.add_command(label = "exit", command = root.quit, accelerator = "Ctrl+q")
+
+# file_menu.bind_all("<Control-o>", open_file)
+# file_menu.bind_all("<Control-s>", save_file)
 
 # Text edit.
 text = tk.Text(root)

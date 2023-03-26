3b3d78ee51becfc1762674a1466939a753ecf702
knish <c0a21099e4@edu.teu.ac.jp>
2023-03-26 10:26:45 +0900

    新規作成機能の追加

--- notepad.py
+++ notepad.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 from tkinter import filedialog as tkf
+from tkinter import messagebox as tkmb
 
 def open_file():
+    global filename
+    if text.edit_modified():
+        save = tkmb.askyesnocancel("Save Changes", "Do you want to save changes before opening a new file?")
+        if save:
+            save_file()
+        elif save is None:
+            return
     filename = tkf.askopenfilename(
         title="Open the txt file.",
         filetypes=[('txt files', '.txt')],
@@ -12,23 +20,38 @@ def open_file():
             text.delete('1.0', tk.END)  # Clear current text.
             text.insert('1.0', f.read())
         print(filename)
-    
 
 def save_file():
-    filename = tkf.asksaveasfilename(
-        title="Save the txt file.",
-        filetypes=[('txt files', '.txt')],
-        initialdir="./",  # Your directory.
-        defaultextension="txt",
-    )
-    if filename:
-        with open(filename, 'w') as f:
-            f.write(text.get("1.0", tk.END))  # Writes input to a text file.
-        print(filename)
+    global filename
+    if not filename:
+        filename = tkf.asksaveasfilename(
+            title="Save the txt file.",
+            filetypes=[('txt files', '.txt')],
+            initialdir="./",  # Your directory.
+            defaultextension="txt",
+        )
+        if not filename:
+            return
+    with open(filename, 'w') as f:
+        f.write(text.get("1.0", tk.END))  # Writes input to a text file.
+    print(filename)
+    text.edit_modified(False)
+
+def new_file():
+    global filename
+    if text.edit_modified():
+        save = tkmb.askyesnocancel("Save Changes", "Do you want to save changes before creating a new file?")
+        if save:
+            save_file()
+        elif save is None:
+            return
+    filename = None
+    text.delete('1.0', tk.END)
+    print("New file created.")
 
 def on_closing():
     if text.edit_modified():  # Check if text has been modified.
-        save = tk.messagebox.askyesnocancel("Save Changes", "Do you want to save changes before quitting?")
+        save = tkmb.askyesnocancel("Save Changes", "Do you want to save changes before quitting?")
         if save:
             save_file()
         elif save is None:
@@ -48,17 +71,20 @@ file_menu = tk.Menu(menu_bar, tearoff=0)  # tearoff(メニューの切り取り�
 menu_bar.add_cascade(label="File", menu=file_menu)
 
 # Crate commands in the file bar.
+file_menu.add_command(label="New", command=new_file, accelerator="Ctrl+N")
 file_menu.add_command(label="Open", command=open_file, accelerator="Ctrl+O")
 file_menu.add_command(label="Save", command=save_file, accelerator="Ctrl+S")
 file_menu.add_separator()  # Separator Line.
 file_menu.add_command(label="Exit", command=root.quit, accelerator="Ctrl+Q")
 
 # Add keybindings to commands in the file bar.
+root.bind("<Control-n>", lambda event: new_file())
 root.bind("<Control-o>", lambda event: open_file())
 root.bind("<Control-s>", lambda event: save_file())
 root.bind("<Control-q>", lambda event: on_closing())
 
 # Text edit.
+filename = None     # ファイルの読み込みと保存両方の機能をつけるのに必要.
 text = tk.Text(root)
 text.pack()
 root.mainloop()
